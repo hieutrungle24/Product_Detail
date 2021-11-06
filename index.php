c18a0828c2d54286fd31f589514e5a201690e17d
hieutrungle24 <hieucalm77@gmail.com>
2021-11-06 22:29:44 +0700

    sanphamct

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include_once("view/header.php");  ?>
+<?php include_once("view/search.php");  ?>
+<?php include_once("view/categories.php");  ?>
+<?php include_once("view/food_menu.php");  ?>
+  
+
+<?php include_once("view/media.php");  ?>
+
+<?php include_once("view/footer.php");  ?>
+
+</body>
+</html>
